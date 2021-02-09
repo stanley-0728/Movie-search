93dbe73692996510d0387360c2327dda337ac9a8
stanley-0728 <74359647+stanley-0728@users.noreply.github.com>
2021-02-09 21:18:58 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const passport=require('passport')
-const User = require('../models/User');
+const User = require('../models/user');
 
 // Login Page
 router.get('/login', (req, res) => res.render('login'));
